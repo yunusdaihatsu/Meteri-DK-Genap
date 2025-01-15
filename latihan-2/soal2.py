502cdd04fe667f46e749fec0022b604a2604b390
azrilrizkiansyah11@gmail.com <azrilrizkiansyah11@gmail.com>
2025-01-15 10:48:24 +0700

    menambahkan latihan 2

---
+++ latihan-2/soal2.py
@@ -0,0 +1,9 @@
+#program menentukan syarat tinggi naik wahana
+
+tinggi=int(input("masukan tinggi siswa :"))
+
+if tinggi > 160 :
+    print ("anda dapat menaiki wahana ini.")
+else:
+    print("anda tidak dapat menaiki wahana ini.")
+    
