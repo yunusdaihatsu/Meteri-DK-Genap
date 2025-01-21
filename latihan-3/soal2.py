3ac139553669881a6c39e1550e969a8bf67d8a84
azrilrizkiansyah <xder414@gmail.com>
2025-01-21 07:49:30 +0700

    menambah looping/perulangan

--- latihan-3/soal2.py
+++ latihan-3/soal2.py
@@ -4,11 +4,11 @@
 print("Program Bonus Toko")
 
 
-nilai=int(input("masukan nilai pembelian pelanggan : "))
+belanja=int(input("masukan nilai pembelian pelanggan : "))
 
-if nilai >= 1500000 and  nilai <= 5000000 :
+if belanja >= 1500000 and belanja  <= 5000000 :
     print("Anda mendapatkan TV Bracket.")
-elif nilai >= 5000000 :
+elif belanja >= 5000000 :
     print("Anda mendapatkan Sound Bar.")
 else:
     print("Tidak Ada Bonus.")
---
+++ latihan-4/soal1.py
@@ -0,0 +1,3 @@
+#program mencetak "selamat datang" sebanyal 100 kali 
+for I in range (10,0,10):
+    print(I)
